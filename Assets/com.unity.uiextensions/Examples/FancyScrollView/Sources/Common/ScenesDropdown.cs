ab8964f7f3ccf4981c6b9f08b5074f3d742864a7
SvetlanaLesnichaia <elsolsueno@gmail.com>
2025-07-13 16:26:29 +0700

    UI Extensions Samples

---
+++ Assets/com.unity.uiextensions/Examples/FancyScrollView/Sources/Common/ScenesDropdown.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * FancyScrollView (https://github.com/setchi/FancyScrollView)
+ * Copyright (c) 2020 setchi
+ * Licensed under MIT (https://github.com/setchi/FancyScrollView/blob/master/LICENSE)
+ */
+
+using System;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+namespace UnityEngine.UI.Extensions.Examples.FancyScrollView
+{
+    [RequireComponent(typeof(Dropdown))]
+    class ScenesDropdown : MonoBehaviour
+    {
+        readonly string[] scenes =
+        {
+            "1024x768",
+            "1280x720",
+            "1920x1080"           
+        };
+
+        void Start()
+        {
+            var dropdown = GetComponent<Dropdown>();
+            dropdown.AddOptions(scenes.Select(x => new Dropdown.OptionData(x)).ToList());
+            dropdown.value = Mathf.Max(0, Array.IndexOf(scenes, SceneManager.GetActiveScene().name));
+            dropdown.onValueChanged.AddListener(value =>
+                SceneManager.LoadScene(scenes[value], LoadSceneMode.Single));
+        }
+    }
+}
