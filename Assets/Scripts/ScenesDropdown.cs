2d21f555ba0fcdde57c77ef5cfd064b4f9a1e2c6
SvetlanaLesnichaia <elsolsueno@gmail.com>
2025-08-08 22:30:44 +0700

    Scene update

---
+++ Assets/Scripts/ScenesDropdown.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * FancyScrollView (https://github.com/setchi/FancyScrollView)
+ * Copyright (c) 2020 setchi
+ * Licensed under MIT (https://github.com/setchi/FancyScrollView/blob/master/LICENSE)
+ */
+
+using System;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+namespace UnityEngine.UI.Extensions.Examples.FancyScrollView
+{
+    [RequireComponent(typeof(Dropdown))]
+    class ScenesDropdown : MonoBehaviour
+    {
+        readonly string[] scenes =
+        {
+            "1920x1080",
+            "1280x720",
+            "1024x768"           
+        };
+
+        void Start()
+        {
+            var dropdown = GetComponent<Dropdown>();
+            dropdown.AddOptions(scenes.Select(x => new Dropdown.OptionData(x)).ToList());
+            dropdown.value = Mathf.Max(0, Array.IndexOf(scenes, SceneManager.GetActiveScene().name));
+            dropdown.onValueChanged.AddListener(value =>
+                SceneManager.LoadScene(scenes[value], LoadSceneMode.Single));
+        }
+    }
+}
